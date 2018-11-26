ac629f44bed1ae839d1156870a7e61600a293f60
pvt5r486 <41603487+pvt5r486@users.noreply.github.com>
2018-11-26 11:23:08 +0800

    test

--- src/components/pages/login.vue
+++ src/components/pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="back-background" style="min-height:100vh;">
+  <div class="back-background">
     <loading :active.sync="isLoading">
       <img src="@/assets/img/loading.gif" alt="" width="200">
     </loading>
@@ -94,5 +94,15 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.container{
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+</style>
 
 
